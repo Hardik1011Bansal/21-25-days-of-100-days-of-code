0011340a20638b92ce83afd9a7f500362318c26f
Hardik1011Bansal <bansalsahab1011@gmail.com>
2025-09-14 21:57:26 +0530

    Create day 25(a).c

---
+++ day 25(a).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter n : ");
+    scanf("%d",&n);
+    if(n<=0)
+    {
+     do
+     {
+       printf("Enter a positive number : ");
+       scanf("%d",&n);
+     } while(n<=0); 
+    }
+    
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=i;j<=5;j++)
+        {
+            printf("%c",'*');
+        }
+        printf("\n");
+    }
+    return 0;
+}
